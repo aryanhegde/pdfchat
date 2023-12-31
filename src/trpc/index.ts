31884444f344feb1dfffeae42b50f98b04918934
unknown <aryanhegde>
2023-12-31 15:39:18 +0530

    trpc and database setup

---
+++ src/trpc/index.ts
@@ -0,0 +1,48 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { privateProcedure, publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
+import { z } from "zod";
+
+export const appRouter = router({
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user?.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    // check if user is in the database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+
+    if (!dbUser) {
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
+
+    return { success: true };
+  }),
+  // GET THE NUMBER OF FILES THAT A USER HAS
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx;
+
+    return await db.file.findMany({
+      where: {
+        userId,
+      },
+    });
+  }),
+  // DELETE A FILE OF USER
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(() => {}),
+});
+
+export type AppRouter = typeof appRouter;
